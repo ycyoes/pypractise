765b7f81a0ca48c578363a049598646da71ce131
ycyoes <ycyoes@163.com>
2020-10-29 13:16:52 +0800

    字符串处理

--- com/turing/test/first/chapter1/name.py
+++ com/turing/test/first/chapter1/name.py
@@ -11,3 +11,7 @@ print(3**2)
 age = 23
 message = "Happy " + str(age) + "rd birthday!"
 print(message)
+
+magicians = ['alice', 'david', 'caroline']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
