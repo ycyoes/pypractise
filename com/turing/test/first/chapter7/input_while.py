1cab74d69173f7a0bf57fde006ab1a5bbb96eaa0
ycyoes <ycyoes@163.com>
2020-10-29 23:21:05 +0800

    input and while

---
+++ com/turing/test/first/chapter7/input_while.py
@@ -0,0 +1,7 @@
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
