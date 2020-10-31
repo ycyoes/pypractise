accb65a008a32491023d8c5a73f3cc0bc2a555b1
ycyoes <ycyoes@163.com>
2020-10-31 18:23:35 +0800

    对象

---
+++ com/turing/foundation/chapter7/obj.py
@@ -0,0 +1,10 @@
+from random import choice
+x = choice(['Hello, world!', [1, 2, 'e', 'e', 4]])
+for i in range(5):
+    print(x)
+
+o = OpenObject()
+o.set_name('Sir Lancelot')
+print(o.get_name())
+
+
