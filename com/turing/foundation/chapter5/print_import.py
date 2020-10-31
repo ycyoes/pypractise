d2030d9084c9eefee0dd9e9d22cdbcbb55a76d6d
ycyoes <ycyoes@163.com>
2020-10-31 15:49:28 +0800

    打印导入

---
+++ com/turing/foundation/chapter5/print_import.py
@@ -0,0 +1,24 @@
+print("I", "wish", "to", "register", "a", "complaint", sep='_')
+
+values = 1, 2, 3
+print(values)
+
+a, b, *rest = [1, 2, 3, 4]
+print(rest)
+
+a, *b, c = "abc"
+print(b)
+
+x = y = [1, 2, 3]
+z = [1, 2, 3]
+print(x == y)
+print(x == z)
+print(x is z)
+
+print("alpha" < "beta")
+
+print(ord('?'))
+print(chr(128584))
+print(ord('B'))
+print(ord('a'))
+print(ord('c'))
