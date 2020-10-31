a8f453ea153a6659077de5c3a21e69fb6849ae0c
ycyoes <ycyoes@163.com>
2020-10-31 22:15:21 +0800

    构造函数

---
+++ com/turing/foundation/chapter9/constructor.py
@@ -0,0 +1,38 @@
+class FooBar:
+    def __init__(self):
+        self.somevar = 42
+
+f = FooBar()
+print(f.somevar)
+
+class Bird:
+    def __init__(self):
+        self.hungry = True
+    def eat(self):
+        if self.hungry:
+            print('Aaaah...')
+            self.hungry = False
+        else: 
+            print('No, thanks!')
+
+b = Bird()
+b.eat()
+b.eat()
+
+class SongBird(Bird):
+    def __init__(self):
+        # Bird.__init__(self)
+        super().__init__()
+        self.sound = 'Squawk!'
+    def sing(self):
+        print(self.sound)
+
+sb = SongBird()
+sb.sing()
+sb.eat()
+sb.eat()    
+
+
+
+
+
