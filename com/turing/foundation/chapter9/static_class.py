a3ccecd4dffa933056837088353356726e0682e9
ycyoes <ycyoes@163.com>
2020-11-02 12:58:18 +0800

    静态类

---
+++ com/turing/foundation/chapter9/static_class.py
@@ -0,0 +1,20 @@
+class MyClass:
+
+    @staticmethod
+    def smeth():
+        print('This is a static method')
+
+    @classmethod
+    def cmeth(cls):
+        print('This is a class method of', cls)
+
+MyClass.smeth()
+MyClass.cmeth()
+
+
+
+
+
+
+
+
