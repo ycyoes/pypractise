640e48b479dcc0a7043ea5b0b385af913d9c2555
ycyoes <ycyoes@163.com>
2020-11-02 12:58:03 +0800

    属性

---
+++ com/turing/foundation/chapter9/attribute.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+    def __setattr__(self, name, value):
+        if name == 'size':
+            self.width, self.height = value
+        else:
+            self.__dict__[name] = value
+    def __getattr__(self, name):
+        if name == 'size': 
+            return self.width, self.height
+        else:
+            raise AttributeError
+
+r = Rectangle()
+print(r.width)
+# r.__setattr__({'name': 'size', 'value': 100})
+print(r.width)
+print(r.__getattr__('size'))
+
+
+
+
+
+
+
+
+
+
+
+
