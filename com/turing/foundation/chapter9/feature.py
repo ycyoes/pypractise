afb9665fe0f9a6a03ccf880942851e1e71cf9d3e
ycyoes <ycyoes@163.com>
2020-11-02 12:58:09 +0800

    特性

---
+++ com/turing/foundation/chapter9/feature.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+    def set_size(self, size):
+        self.width, self.height = size
+    def get_size(self):
+        return self.width, self.height
+    size = property(get_size, set_size)
+
+r = Rectangle()
+r.width = 10
+r.height = 5
+print(r.get_size())
+
+r.set_size((150, 100))
+print(r.width)
+
+
+
+
+
