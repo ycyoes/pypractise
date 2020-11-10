b09b9400ce5017fa8b412eaab49944b1f6469809
ycyoes <ycyoes@163.com>
2020-11-10 21:34:58 +0800

    列表

---
+++ com/turing/fromzero/chapter2/list_test.py
@@ -0,0 +1,14 @@
+
+#count
+field = list('hello, world')
+print(field)
+print(field.count('o'))
+
+#extend
+a = ['hello', 'world']
+b = ['python', 'is', 'funny']
+a.extend(b)
+print(a)
+
+#index
+print(a.index('hello'))
