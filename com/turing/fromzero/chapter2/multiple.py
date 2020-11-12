1d5099fb45f622de511e1f8087ce7d08746dd1a7
ycyoes <ycyoes@163.com>
2020-11-12 19:39:20 +0800

    九九乘法表

--- com/turing/fromzero/chapter2/multiple.py
+++ com/turing/fromzero/chapter2/multiple.py
@@ -1,10 +1,10 @@
 
 #九九乘法表
 for i in range(1, 10):
-   
-    for j in range(1, 10):
+    for j in range(1, i+1):
         # print(i + "*" + j + "=" + i * j)
-        print(i,"*", j , "=", i * j, end="")
-        print("\t", end="")
+        # print(i,"*", j , "=", i * j, end="")
+        # print("\t", end="")
+        print("%d*%d=%2d" % (i, j, i * j), end=" ")
         # print(i, j)
     print()
