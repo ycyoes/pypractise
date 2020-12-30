70d76aa7e9242764a9981537ffd378ba9fd4002b
ycyoes <ycyoes@163.com>
2020-12-30 14:40:47 +0800

    myfunc

---
+++ com/turing/ml/chapter01/myfunc.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def myfunc(a):
+    useless_number = np.random.randn(1)
+    print(useless_number)
+    b = a
+    c = call_func(b)
+    print(1 / c)
+
+
+def call_func(x):
+    return x
+
+
+if __name__ == '__main__':
+    myfunc(10)
+    myfunc(0)
