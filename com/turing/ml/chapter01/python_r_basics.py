ec01b0531719fa65ff915705aaeb4da1517158c4
ycyoes <ycyoes@163.com>
2020-12-30 16:50:48 +0800

    class Failure

--- com/turing/ml/chapter01/python_r_basics.py
+++ com/turing/ml/chapter01/python_r_basics.py
@@ -53,7 +53,24 @@ logging.info("This is some useful information.")
 logging.warning("This is some warning!")
 logging.error("Something went wrong!")
 
-
+class Failure():
+    def __init__(self, value, failed=False):
+        self.value = value
+        self.failed = failed
+    def get(self):
+        return self.value
+    def is_failed(self):
+        return self.failed
+    def __str__(self):
+        return ' '.join([str(self.value), str(self.failed)])
+    def __or__(self, f):
+        if self.failed:
+            return self
+        try:
+            x = f(self.get())
+            return Failure(x)
+        except:
+            return Failure(None, True)
 
 
 
