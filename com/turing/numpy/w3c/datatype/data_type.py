ff8793a02b03196418cc1daa8f6bf4d1404ee244
ycyoes <ycyoes@163.com>
2021-01-05 16:40:21 +0800

    convert of data type

--- com/turing/numpy/w3c/datatype/data_type.py
+++ com/turing/numpy/w3c/datatype/data_type.py
@@ -34,9 +34,26 @@ arr1 = np.array([1,2,3,4], dtype='S')
 print(arr1)
 print(arr1.dtype)
 
+arr2 = np.array(['apple', 'banana', 'cherry'])
+print(arr2)
+print(arr2.dtype)
 
+#Create an array with data type 4 bytes integer
+arr3 = np.array([1,2,3,4], dtype='i4')
+print(arr3)
+print(arr3.dtype)
 
+# Change data type from float to integer by using 'i' as parameter value:
+arr4 = np.array([1.1, 2.1, 3.1])
 
+newarr = arr4.astype('i')
 
+print(newarr)
+print(newarr.dtype)
 
+arr5 = np.array([1, 0, 3, -1, 0])
 
+newarr = arr5.astype(bool)
+
+print(newarr)
+print(newarr.dtype)
