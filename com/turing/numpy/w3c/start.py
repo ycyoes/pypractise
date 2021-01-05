60f8a9ec5d58b994c6bd05bf7b13e6ee2e1b2185
ycyoes <ycyoes@163.com>
2021-01-05 15:39:53 +0800

    Use a tuple to create a NumPy array

--- com/turing/numpy/w3c/start.py
+++ com/turing/numpy/w3c/start.py
@@ -1,3 +1,25 @@
 import numpy as np
 arr = np.array([1,2,3,4,5])
 print(arr)
+
+print(np.__version__)
+
+print(type(arr))
+
+# Use a tuple to create a NumPy array:
+arr1 = np.array((1,2,3,4,5))
+print(arr1)
+
+arr2 = np.array(42)
+print(arr2)
+
+
+
+
+
+
+
+
+
+
+
