6d7bb306b7b73386a6147ff3638658cf66b321f0
ycyoes <ycyoes@163.com>
2021-01-10 17:45:21 +0800

    shape of tensor

--- com/turing/numpy/common_use/shape_tensor.py
+++ com/turing/numpy/common_use/shape_tensor.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 x = np.random.randn(10, 5)
-print(x[:, None, :].shape) # This will add a new dimension
+print(x[:, None, :].stgfhape) # This will add a new dimension
 
 print(np.expand_dims(x, 1).shape)
