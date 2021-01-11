1800ee9da98106bf580efdcec39246c4d2e518c4
ycyoes <ycyoes@163.com>
2021-01-11 15:46:48 +0800

    select two columns

--- com/turing/pandas/dataframe.py
+++ com/turing/pandas/dataframe.py
@@ -17,5 +17,18 @@ df = pd.DataFrame(data)
 #Print the output
 print(df)
 
+print('-----dealing with rows and columns---------')
+# Define a dictionary containing employee data
+data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Age': [27, 24, 22, 32],
+        'Address': ['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+        'Qualification': ['Msc', 'MA', 'MCA', 'Phd']}
+
+# Convert the dictionary into DataFrame
+df = pd.DataFrame(data)
+
+# select two columns
+print(df[['Name', 'Qualification', 'Age']])
+
 
 
