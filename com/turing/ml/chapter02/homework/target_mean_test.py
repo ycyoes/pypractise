e9ef88a4c702c5ef03f708fcc9a49944f2b46e1a
ycyoes <ycyoes@163.com>
2021-01-12 09:46:19 +0800

    target encoding test

---
+++ com/turing/ml/chapter02/homework/target_mean_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+
+def main():
+    print('test')
+
+
+
+if __name__ = '__main__':
+    main()
+
+
+
--- com/turing/ml/chapter02/homework/test.py
+++ com/turing/ml/chapter02/homework/test.py
@@ -9,9 +9,19 @@ def target_mean_v1(data, y_name, x_name):
     return result
 
 def main():
-    y = np.random.randint(2, size=(5000, 1))
-    x = np.random.randint(10, size=(5000, 1))
+    y = np.random.randint(2, size=(50, 1))
+    x = np.random.randint(10, size=(50, 1))
     data = pd.DataFrame(np.concatenate([y, x], axis=1), columns=['y', 'x'])
+    print('data\n', data)
+    result = np.zeros(data.shape[0])
+    for i in range(data.shape[0]):
+        # print('i=', i, (data.index != i), '\n', data[data.index != i])
+        groupby_result = data[data.index != i].groupby(['x'], as_index=False).agg(['mean', 'count'])
+        print('groupby_result: ', groupby_result, ' :end')
+        result[i] = groupby_result.loc[groupby_result.index == data.loc[i, 'x'], ('y', 'mean')]
+        print('i: ', i, result[i])
+
+
     result_1 = target_mean_v1(data, 'y', 'x')
     # result_2 = target_mean_v2(data, 'y', 'x')
     print(result_1)
