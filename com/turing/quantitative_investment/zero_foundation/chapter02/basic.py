d06e15b83ff157e215fad7744d9f9ba3293c9186
ycyoes <ycyoes@163.com>
2021-01-16 18:39:36 +0800

    quantitative investment basic

---
+++ com/turing/quantitative_investment/zero_foundation/chapter02/basic.py
@@ -0,0 +1,22 @@
+
+#character of line change
+a = 15
+if a >= 10:
+    print("a is larger than 10!")
+else:
+    print("a is smaller than 10")
+
+print("good good study, \
+      day day up!")
+
+
+dir = dir(__builtins__)
+print(dir)
+
+
+
+
+
+
+
+
