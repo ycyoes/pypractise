7f6d908330378744270fb603cec82e8b5e049b2a
ycyoes <ycyoes@163.com>
2021-03-25 17:49:13 +0800

    列表与元组

--- com/turing/advanced/chapter2/innertype.py
+++ com/turing/advanced/chapter2/innertype.py
@@ -1,2 +1,16 @@
 print('-------------chapter2--------')
 print(bytes([102,111,111]))
+
+print(list(b'foo bar'))
+print(tuple(b'foo bar'))
+
+print(type("some string"))
+
+print(type(b"some bytes"))
+
+print(','.join(['some', 'comma', 'separated', 'values']))
+
+# 1．列表与元组
+
+
+
