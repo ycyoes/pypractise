daf25bb87d645521872ac5ced1fe2734c9cb655c
ycyoes <ycyoes@163.com>
2021-03-25 17:59:48 +0800

    enumerate（枚举）

--- com/turing/advanced/chapter2/innertype.py
+++ com/turing/advanced/chapter2/innertype.py
@@ -11,6 +11,11 @@ print(type(b"some bytes"))
 print(','.join(['some', 'comma', 'separated', 'values']))
 
 # 1．列表与元组
+# 列表推导
+print([i for i in range(10) if i % 2 == 0])
 
+# enumerate（枚举）
+for i, element in enumerate(['one', 'two', 'three']):
+        print(i, element)
 
 
