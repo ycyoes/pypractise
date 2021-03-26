5737f443edce0bb004e952be4f8a4bf6d2ef2eaf
ycyoes <ycyoes@163.com>
2021-03-26 18:23:12 +0800

    利用next函数与调用的代码进行交互。yield变成了一个表达式，而值可以通过名为send的新方法来传递

--- com/turing/advanced/chapter2/grammer.py
+++ com/turing/advanced/chapter2/grammer.py
@@ -55,6 +55,20 @@ results = adder(power(elements))
 print(next(results))
 print(next(results))
 
+def psychologist():
+    print('Please tell me your problems')
+    while True:
+        answer = (yield)
+        if answer is not None:
+            if answer.endswith('?'):
+                print("Don't ask yourself too much questions")
+            elif 'good' in answer:
+                print("Ahh that's good, go on")
+            elif 'bad' in answer:
+                print("Don't be so negative")
+
+free = psychologist()
+print(next(free))
 
 
 
