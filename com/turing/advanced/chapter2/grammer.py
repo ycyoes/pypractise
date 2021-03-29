3823f954cf907ba5b1631254318c6ac94cd071b8
ycyoes <ycyoes@163.com>
2021-03-29 18:23:37 +0800

    定义装饰器-参数检查-'NoneType' object is not callable

--- com/turing/advanced/chapter2/grammer.py
+++ com/turing/advanced/chapter2/grammer.py
@@ -236,13 +236,50 @@ my = RPCView()
 my.meth1(1,2)
 
 
+# 缓存
+import time
+import hashlib
+import pickle
+
+cache = {}
 
 
+def is_obsolete(entry, duration):
+    return time.time() - entry['time'] > duration
 
 
+def compute_key(function, args, kw):
+    key = pickle.dumps((function.__name__, args, kw))
+    return hashlib.sha1(key).hexdigest()
 
 
+def memoize(duration=10):
+    def _memoize(function):
+        # print('----------function-----------\n', function)
+        def __memoize(*args, **kw):
+            key = compute_key(function, args, kw)
 
+            # 是否已经拥有它了？
+            if(key in cache and not is_obsolete(cache[key], duration)):
+                print('we got a winner')
+                return cache[key]['value']
+            # 计算
+                result = function(*args, **kw)
+                # 保存结果
+                cache[key] = {
+                    'value': result,
+                    'time': time.time()
+                }
+                return result
+            return __memoize
+        return _memoize
+
+@memoize
+def very_very_very_complex_stuff(a, b):
+    return a + b
+
+
+very_very_very_complex_stuff(2, 2)
 
 
 
