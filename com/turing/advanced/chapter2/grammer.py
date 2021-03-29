7ecd56cb79134659f643e3715a77e81cf28a9124
ycyoes <ycyoes@163.com>
2021-03-29 10:09:38 +0800

    定义装饰器-保存内省的装饰器

--- com/turing/advanced/chapter2/grammer.py
+++ com/turing/advanced/chapter2/grammer.py
@@ -151,8 +151,17 @@ def bar():
 
 bar()
 
+def dummy_decorator(function):
+    def wrapped(*args, **kwargs):
+        """包装函数内部文档。"""
+        return function(*args, **kwargs)
+    return wrapped
 
+@dummy_decorator
+def function_with_important_docstring():
+    """这是我们想要保存的重要文档字符串。"""
 
-
+print(function_with_important_docstring.__name__)
+print(function_with_important_docstring.__doc__)
 
 
