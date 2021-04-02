2fdc800e17759919a47b7f48c0aff39cbd050bda
ycyoes <ycyoes@163.com>
2021-04-02 17:54:32 +0800

    不定参数使用*arg来表示，而arg实际是一个元组（tuple）

--- com/turing/lxf/foundation/function.py
+++ com/turing/lxf/foundation/function.py
@@ -25,4 +25,31 @@ print(vol)
 
 print("%s, %s, %d" % ("你好", "中国", 2018))
 
+def printf(format, *arg):
+    print(format%arg)
+
+printf("年后")
+
+printf("%s,%s", "你好", "中国")
+printf("%s, %s, %d" % ("你好", "中国", 2018))
+
+def getchange(*arg):
+    print(arg)
+
+getchange(1,2,3)
+
+def addall(*arg):
+    total = 0
+    for arg_one in arg:
+        total += arg_one
+    return total
+
+aa = addall(1,2,3,4)
+print(aa)
+
+
+
+
+
+
 
