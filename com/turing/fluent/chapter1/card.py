7391a1175469a8060181f815c1d56c9d3ad33930
ycyoes <ycyoes@163.com>
2021-07-09 11:31:11 +0800

    sort

--- com/turing/fluent/chapter1/card.py
+++ com/turing/fluent/chapter1/card.py
@@ -16,3 +16,18 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self._cards[position]
 
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+
+
+
+
+
+
+
+
